8a7cfe8c97f1e2571292bf3ccb69a242b07c8172
Kd5ahl <kd5ahl@gmail.com>
2018-02-16 08:40:43 -0500

    Add files via upload

---
+++ WineCoolerTemp.ino
@@ -0,0 +1,106 @@
+#include <ESP8266WiFi.h>
+
+#include <PubSubClient.h>
+
+#include <OneWire.h>
+
+#include <DallasTemperature.h>
+
+// Pin Mapping
+// Wemos / Arduino
+// D0 = 16
+// D1 = 5
+// D2 = 4
+// D3 = 0
+// D4 = 2
+// D5 = 14
+// D6 = 12
+// D7 = 13
+// D8 = 15
+
+
+
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 14
+
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+// Update these with values suitable for your network.
+const char* ssid = "PeggoMyEggo2";
+const char* password = "scarlet begonias";
+const char* mqtt_server = "192.168.5.201";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+float temp = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  WiFi.hostname("ENV-G-FREEZER");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("WineCoolerTempSensor")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  setup_wifi(); 
+  client.setServer(mqtt_server, 1883);
+  sensors.begin();
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
+    sensors.setResolution(12);
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    temp = sensors.getTempFByIndex(0);
+    Serial.println(temp);
+    if((temp > -20) && (temp <160))
+      {
+      client.publish("home/sensors/temperature/g-freezer", String(temp).c_str(),TRUE);
+      }
+  }
+}
